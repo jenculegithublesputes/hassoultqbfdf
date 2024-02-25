cd242f5f0e6bbdace016a48141068be512304ec0
jenculegithublesputes <161207878+jenculegithublesputes@users.noreply.github.com>
2024-02-25 17:20:14 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,23 +1,29 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlretrieve
 from concurrent.futures import ThreadPoolExecutor
 
 # Vérifier si le dossier 'videos' existe, sinon le créer
 if not os.path.exists('videos'):
     os.makedirs('videos')
 
+# Obtenir le nombre de pages à scraper depuis l'utilisateur
+num_pages = int(input("Entrez le nombre de pages à scraper: "))
+
 # Code pour extraire les liens des pages de la première URL
-url = 'https://hotscope.tv/category/masturbation?page=1'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+base_url = 'https://hotscope.tv/category/masturbation?page='
 
 links = []
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href.startswith('/periscope') or href.startswith('/snapchat') or href.startswith('/onlyfans') or href.startswith('/porn'):
-        links.append("https://hotscope.tv" + href)
+for page in range(1, num_pages + 1):
+    url = base_url + str(page)
+    print(page)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('/periscope') or href.startswith('/snapchat') or href.startswith('/onlyfans') or href.startswith('/porn'):
+            links.append("https://hotscope.tv" + href)
 
 # Utiliser ThreadPoolExecutor pour récupérer les liens en parallèle
 def fetch_link_content(link):
@@ -32,7 +38,7 @@ def fetch_link_content(link):
     
     return video_links
 
-with ThreadPoolExecutor(max_workers=5) as executor:
+with ThreadPoolExecutor(max_workers=20) as executor:
     all_video_links = list(executor.map(fetch_link_content, links))
 
 # Flatten the list of lists
@@ -42,26 +48,25 @@ with open('links.txt', 'w') as file:
     for link in video_links:
         file.write(link + '\n')
 
-# Lire les URL à partir du fichier links.txt
-with open('links.txt', 'r') as file:
-    urls = file.read().splitlines()
-
 # Utiliser ThreadPoolExecutor pour télécharger en parallèle
 def download_video(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
 
-    # Trouver les balises vidéo et extraire les liens
-    for video in soup.find_all('video'):
-        src = video.get('src')
-        if src and src.startswith('https://cdn.hotscope.tv/files/') and '.mp4' in src:
-            # Nom du fichier basé sur le dernier segment du lien
-            filename = os.path.join('videos', src.split('/')[-1])
+        # Nom du fichier basé sur le dernier segment du lien
+        filename = os.path.join('videos', url.split('/')[-1])
 
-            # Télécharger le fichier dans le dossier 'videos'
-            urlretrieve(src, filename)
-            print(f'Téléchargé : {filename}')
+        # Télécharger le fichier dans le dossier 'videos'
+        with open(filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
+
+        print(f'Téléchargé : {filename}')
+    except requests.exceptions.RequestException as e:
+        print(f'Erreur de téléchargement pour {url}: {e}')
 
 # Utiliser ThreadPoolExecutor pour télécharger en parallèle
-with ThreadPoolExecutor(max_workers=5) as executor:
-    executor.map(download_video, urls)
+with ThreadPoolExecutor(max_workers=20) as executor:
+    executor.map(download_video, video_links)
